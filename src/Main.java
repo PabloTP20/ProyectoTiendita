133669bad263ea3ac8a7ff911ef12046d3f6c19b
jbetogarciaparedescreate <jbetogarciaparedes@gmail.com>
2025-09-27 18:42:31 -0600

    Se agrega segundo colaborador

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hola mundo");
+        System.out.println("Hola mundo");
         System.out.println("Torres Priego Pablo");
+        System.out.println("Garcia Paredes Jesus Alberto");
     }
 }
